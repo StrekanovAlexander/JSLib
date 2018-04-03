2dad4a3f5ece74592a46d10dedc65d492d7613f2
StrekanovAlexander <strekanov.alexander@gmail.com>
2018-04-03 15:13:50 +0300

    list six release

--- list.js
+++ list.js
@@ -1,47 +1,74 @@
+const square = x => x * x;
+const isEven = x => x % 2 === 0;
+/********************************/
 const cons = (elem, list) => msg => {
     switch(msg) {
         case 'head':
             return elem;
         case 'tail':
-            return list;
-    }
+            return list;    
+    } 
 };
 
 const head = list => list('head');
 const tail = list => list('tail');
-
 const isEmpty = list => list === null;
-const emptyList = null;
-const square = x => x * x;
-const isEven = x => x % 2 === 0; 
-const add = (a, b) => a + b;
 
 const toString = list => {
     const iter = (items, acc) => {
         if (isEmpty(items)) return acc;
-        return iter(tail(items), `${acc}${head(items)}`);
-    };
-    return iter(list, '');    
+        return iter(tail(items), `${acc} ${head(items)}`); 
+    }     
+    return iter(list, ``);  
 };
 
 const map = (f, list) => {
     if (isEmpty(list)) return null;
-    return cons(f(head(list)) , map(f, tail(list))); 
+    return cons(f(head(list)), map(f, tail(list)));
 };
 
 const filter = (f, list) => {
-    if (isEmpty(list)) return null; 
-    if (f(head(list))) {
+    if (isEmpty(list)) return null;
+    if (f(head(list))){
         return cons(head(list), filter(f, tail(list)));
     } else {
-        return filter(f, tail(list));
+        return filter(f, tail(list)); 
     }
+}; 
+
+const reduce = (f, acc, list) => {
+    if(isEmpty(list)) return acc;
+    if (f(head(list))) {
+        acc += 1;
+    }; 
+    return reduce(f, acc, tail(list)); 
 };
 
-const not = x => x ? false : true;
-const and = (a, b) => a ? ( b ? true : false) : false; 
-const or = (a, b) => {
-    if (a) return true;
-    if (b) return true;
-    return false;
-};   
+const reduce2 = (f, acc, list) => {
+    if(isEmpty(list)) return acc;
+    if (f(head(list))) {
+       acc = cons(head(list), acc);
+    }; 
+    return reduce2(f, acc, tail(list)); 
+};
+
+const reverse = list => {
+    const iter = (items, acc) => {
+        if (isEmpty(items)) return acc;
+        return iter(tail(items), cons(head(items), acc));
+    }   
+    return iter(list, null); 
+}
+
+/* const numbers = cons(1, cons(2, cons(3, cons(4, cons(5, cons(6, cons(7, null)))))));
+const numbers2 = map(square, numbers);
+const numbers3 = filter(isEven, numbers); 
+const numbers4 = reduce2(isEven, null, numbers);  
+const numbers5 = reverse(numbers4);   
+
+console.log(toString(numbers));
+console.log(toString(numbers2));
+console.log(toString(numbers3));
+console.log(toString(numbers4));
+console.log(toString(numbers5));
+ */
