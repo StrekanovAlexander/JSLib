ab36eb39f67b9eb9a644095b884591e0a96eea22
StrekanovAlexander <strekanov.alexander@gmail.com>
2018-04-06 14:56:21 +0300

    list ten release

--- list.js
+++ list.js
@@ -1,83 +1,36 @@
-const square = x => x * x;
-const isEven = x => x % 2 === 0;
-/********************************/
-const cons = (elem, list) => msg => {
-    switch(msg) {
-        case 'head':
-            return elem;
-        case 'tail':
-            return list;    
-    } 
-};
+const True = x => y => x;
+const False = x => y => y;
+const If = b => x => y => b(x)(y);
 
-const head = list => list('head');
-const tail = list => list('tail');
-const isEmpty = list => list === null;
+const cons = (x, y) => b => b(x)(y);
+const car = pair => pair(True);
+const cdr = pair => pair(False);
+const isEmpty = x => x === null;
 
 const toString = list => {
     const iter = (items, acc) => {
         if (isEmpty(items)) return acc;
-        return iter(tail(items), `${acc} ${head(items)}`); 
-    }     
-    return iter(list, ``);  
+        return iter(cdr(items), `${acc} ${car(items)}`);  
+    };
+    return iter(list, ``);
 };
 
 const map = (f, list) => {
     if (isEmpty(list)) return null;
-    return cons(f(head(list)), map(f, tail(list)));
+    return cons(f(car(list)), map(f, cdr(list))); 
 };
 
 const filter = (f, list) => {
     if (isEmpty(list)) return null;
-    if (f(head(list))){
-        return cons(head(list), filter(f, tail(list)));
+    if (f(car(list))){
+        return cons(car(list), filter(f, cdr(list)));
     } else {
-        return filter(f, tail(list)); 
+        return filter(f, cdr(list));
     }
-}; 
+};
 
 const reduce = (f, acc, list) => {
-    if(isEmpty(list)) return acc;
-    if (f(head(list))) {
-        acc += 1;
-    }; 
-    return reduce(f, acc, tail(list)); 
-};
-
-const reduce2 = (f, acc, list) => {
-    if(isEmpty(list)) return acc;
-    if (f(head(list))) {
-       acc = cons(head(list), acc);
-    }; 
-    return reduce2(f, acc, tail(list)); 
-};
-
-const reverse = list => {
-    const iter = (items, acc) => {
-        if (isEmpty(items)) return acc;
-        return iter(tail(items), cons(head(items), acc));
-    }   
-    return iter(list, null); 
-};
-
-const has = (elem, list) => {
-    if (isEmpty(list)) return false;
-    if (head(list) === elem) return true;
-    return has(elem, tail(list)); 
-};
-
-const numbers = cons(1, cons(2, cons(3, cons(4, cons(5, cons(6, cons(7, null)))))));
-const numbers2 = cons(1, cons(2, cons(3, cons(4, cons(2, cons(6, cons(7, null)))))));
-
-//console.log(toString(numbers2));
-
-const union = list => {
-    const iter = (items, acc) => {
-        if (isEmpty(items)) return acc;
-        if (has(head(items), acc)){
-            return iter(tail(items), acc);
-        }
-        return iter(tail(items), cons(head(items), acc)); 
-    }
-    return iter(list, null);
+    if (isEmpty(list)) return acc;
+    acc = f(car(list)) ? acc + 1 : acc;
+    return reduce(f, acc, cdr(list));
 }
